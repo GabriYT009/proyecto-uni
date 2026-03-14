22570b8c48417688807317ddc142b9dddf9509fb
Gabox <durangogabriel8@gmail.com>
2026-03-14 12:53:58 -0500

    Delete: se elimina la carpeta pantalla de todo el proyecto ya que no se estaba usando para nada

--- pantalla/pantalla/django_app/homepage/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class HomepageConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    # The app is nested under the django_app package.
-    name = 'django_app.homepage'
--- pantalla/pantalla/django_app/homepage/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'homepage/index.html', {})
-
-# Create your views here.
