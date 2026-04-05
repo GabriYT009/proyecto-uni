993d5196f50ab2bfe5086bd0bd72651bc27ee136
cristian <tu_correo@ejemplo.com>
2026-04-04 23:45:16 -0400

    asd

--- backend/django_app/core/bcv.py
+++ backend/django_app/core/bcv.py
@@ -3,7 +3,7 @@ import requests
 
 def obtener_tasa_cambio():
     URL= "https://api.dolarvzla.com/public/bcv/exchange-rate"
-    MI_API_KEY= "e76421c6acdd43d60e5a8b01496517028fd712381d17f6785ad6b0c2ab8b2db9"
+    MI_API_KEY= "eeb48306e200fe4256ad51f5bdf6e2cbd52dc6aab667b46a5c54b02be2332881"
     HEADERS = {"x-dolarvzla-key": MI_API_KEY}
 
     response = requests.get(URL,headers=HEADERS)
