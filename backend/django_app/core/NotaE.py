68dcdfd26e3f9ab4135c9b96a0f1e86c20827de3
Potatuwu820 <cristian4137h@gmail.com>
2026-05-03 01:49:28 -0400

    asd

--- backend/django_app/core/NotaE.py
+++ backend/django_app/core/NotaE.py
@@ -111,7 +111,7 @@ class Generar_NE(FPDF):
             
             # 2. Tomamos el precio que guardaste en el carrito (o del producto si falla)
             precio_unit = item.precio_unitario or producto.precio_venta or 0
-            precio_unit_bs = precio_unit * tasa
+            precio_unit_bs = float(precio_unit) * tasa
             # 3. Como no tienes un campo 'sub_total_item', lo calculamos aquí mismo:
             subtotal = float(cantidad) * float(precio_unit)
             
