5e00ef851fe5c505212b008666436546d3f3f0b4
Potatuwu820 <cristian4137h@gmail.com>
2026-05-03 01:47:24 -0400

    asd

--- backend/django_app/core/NotaE.py
+++ backend/django_app/core/NotaE.py
@@ -97,7 +97,12 @@ class Generar_NE(FPDF):
         
         # --- AQUÍ ESTABA EL ERROR: Declarar total_usd en lugar de total ---
         total_usd = 0
+        tasa=""
+        try:
+            tasa=obtener_tasa_cambio()
 
+        except Exception:
+            tasa=0
         for item in self.items:
             producto = item.Producto
             
@@ -106,7 +111,7 @@ class Generar_NE(FPDF):
             
             # 2. Tomamos el precio que guardaste en el carrito (o del producto si falla)
             precio_unit = item.precio_unitario or producto.precio_venta or 0
-            precio_unit_bs = precio_unit *obtener_tasa_cambio()
+            precio_unit_bs = precio_unit * tasa
             # 3. Como no tienes un campo 'sub_total_item', lo calculamos aquí mismo:
             subtotal = float(cantidad) * float(precio_unit)
             
