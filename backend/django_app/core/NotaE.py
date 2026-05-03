40c535da9d9292cbb46e8fc960f880095c2239bd
Potatuwu820 <cristian4137h@gmail.com>
2026-05-03 02:18:29 -0400

    asd

--- backend/django_app/core/NotaE.py
+++ backend/django_app/core/NotaE.py
@@ -88,10 +88,10 @@ class Generar_NE(FPDF):
         self.set_font('Arial', 'B', 10)
         self.cell(60, 10, 'Producto', 1, 0, 'L')
         self.cell(17, 10, 'Cant.', 1, 0, 'C')
-        self.cell(30, 10, 'Precio Unit.', 1, 0, 'c')
+        self.cell(25, 10, 'Precio Unit.', 1, 0, 'c')
         self.cell(25, 10, 'Precio Unit Bs.', 1, 0, 'c')
-        self.cell(20, 10, 'Subtotal', 1, 1, 'R')
-        self.cell(20, 10, 'Subtotal Bs', 1, 1, 'R')
+        self.cell(20, 10, 'Subtotal', 1, 1, 'c')
+        self.cell(20, 10, 'Subtotal Bs', 1, 1, 'c')
         # Table content
         self.set_font('Arial', '', 10)
         
