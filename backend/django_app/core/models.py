367488019793f1eec7d521869f4fd705401dc395
cristian <tu_correo@ejemplo.com>
2026-05-31 16:09:36 -0400

    asd

--- backend/django_app/core/models.py
+++ backend/django_app/core/models.py
@@ -63,7 +63,7 @@ class Categoria(models.Model):
     
     # Campos nuevos según SQL
     # rif_proveedor = models.CharField(max_length=45, blank=True, null=True)
-    # telefono_proveedor = models.CharField(max_length=45, blank=True, null=True)
+    #  telefono_proveedor = models.CharField(max_length=45, blank=True, null=True)
     # direccion_proveedor = models.CharField(max_length=100, blank=True, null=True)
 
     class Meta:
