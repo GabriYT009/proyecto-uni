97f0d5ce228883b985cb5f0b3d28a9e968a204e1
Potatuwu820 <cristian4137h@gmail.com>
2026-06-14 00:06:07 -0400

    asd

--- backend/django_app/core/NotaE.py
+++ backend/django_app/core/NotaE.py
@@ -164,7 +164,7 @@ class Generar_NE(FPDF):
                 self.set_font('Arial', 'B', 12)
 
         self.cell(150, 10, 'TOTAL USD:', 0, 0, 'R')
-        self.cell(30, 10, f'${total_final_usd:.2f}', 0, 1, 'C')
+        self.cell(30, 10, f'{total_final_usd:.2f}', 0, 1, 'C')
         total_bs = total_final_usd * tasa
         self.cell(150, 10, 'TOTAL BS:', 0, 0, 'R')
         self.cell(30, 10, f'{total_bs:.2f}', 0, 1, 'C')
@@ -173,7 +173,7 @@ class Generar_NE(FPDF):
             try:
                 total_bs = total_final_usd * float(self.salida.bcv)
                 self.set_font('Arial', 'B', 12)
-                self.cell(150, 10, f'Tasa BCV: {self.salida.bcv}', 0, 0, 'C')
+                self.cell(150, 10, f'Tasa BCV: {self.salida.bcv}', 0, 0, 'R')
                 self.cell(30, 10, '', 0, 1, 'R')
     
             except (ValueError, TypeError):
